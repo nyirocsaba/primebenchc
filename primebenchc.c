01346c580447e9ced363e11cd6727f1cf6d10830
nyirocsaba <nyirocsaby@gmail.com>
2013-02-26 19:57:35 +0200

    Improve configurations implemeting arguments handling via getopt_long

--- primebenchc.c
+++ primebenchc.c
@@ -21,10 +21,33 @@ http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #include <pthread.h>
 #include <sys/time.h>
 #include <math.h>
+#include <getopt.h>
+
+#define _PRIMEBENCHC_VERSION "v0.2"
+
+
+#define handle_error_en(en, msg) \
+       do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
 long sum, numbersPerProcess;
 pthread_mutex_t sum_mutex;
 
+static void usage(char *prog_name, char *msg)
+{
+   if (msg != NULL)
+       fputs(msg, stderr);
+
+   fprintf(stderr, "Usage: %s [options]\n", prog_name);
+   fprintf(stderr, "Options are:\n");
+   #define fpe(msg) fprintf(stderr, "\t%s", msg);          /* Shorter */
+   fpe("--im <numeric>   Set intervcal maximum\n");
+   fpe("--tmin <numeric> Set the minimum number of threads\n");
+   fpe("--tmax <numeric> Set the maximum number of threads\n");
+   fpe("--showlicense    List license message\n");
+   fpe("--hidelicense    Don't list license message\n");
+   fprintf(stderr, "Defaults: %s --im 100000 --tmin 1 --tmax 8 --showlicense\n", prog_name);
+   exit(EXIT_FAILURE);
+}
 
 int isPrime(long num) {
     if (num<3) {
@@ -83,6 +106,8 @@ void calculateThreaded(long threadcount, long until) {
             exit(-1);
         }
     }
+
+    pthread_attr_destroy(&attr);
     void *status;
     pthread_attr_destroy(&attr);
     for(i=0; i<threadcount; i++) {
@@ -97,25 +122,55 @@ void calculateThreaded(long threadcount, long until) {
 
 int main(int argc, char *argv[])
 {
-    printf("All primebenchc code is Copyright 2013 by Nyiro Zoltan-Csaba.\n\nThis program is free software; you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation; either version 2 of the License, or (at\nyour option) any later version.\n\nThis program is distributed in the hope that it will be useful, but\nWITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY\nor FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License\nfor more details.\n\n");
-    printf("You should have received a copy of the GNU General Public License\nalong with this program as the file LICENSE.txt; if not, please see\nhttp://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.\n\n\n");
-    if (argc<3) {
-        printf("Wrong arguments!\nUseage: primebenchc [interval top value] [max number of threads]\nprimebenchc [interval top value] [min number of threads] [max number of threads]");
-        return 0;
-    }
-    long until = atol(argv[1]);
+    long until = 100000;
     int threadstart = 1;
-    int threadcount;
-    if (argc>3) {
-        threadstart = atoi(argv[2]);
-        threadcount = atoi(argv[3]);
-    } else {
-        threadcount = atoi(argv[2]);
+    int threadcount = 8;
+    static int listlicense = 1;
+
+    static struct option long_options[] = {
+                   {"showlicense", no_argument, &listlicense, 1},
+                   {"hidelicense", no_argument, &listlicense, 0},
+                   {"im",   required_argument, 0, 'i'},
+                   {"tmin", required_argument, 0, 'c'},
+                   {"tmax", required_argument, 0, 'f'}
+    };
+    int opt;
+    int option_index = 0;
+
+    while ((opt = getopt_long(argc, argv, "i:c:f:",long_options, &option_index))!=-1) {
+        switch (opt) {
+            case 0: break;
+            case 'i':
+                until = atol(optarg);
+                if (until<2) {
+                    usage(argv[0], "\nInvalid --im value!\n\n");
+                }
+                break;
+            case 'c':
+                threadstart = atol(optarg);
+                if (threadstart<1) {
+                    usage(argv[0], "\nInvalid --tmin value!\n\n");
+                }
+                break;
+            case 'f':
+                threadcount = atol(optarg);
+                if (threadcount<1) {
+                    usage(argv[0], "\nInvalid --tmax value!\n\n");
+                }
+                break;
+            default:  usage(argv[0], NULL);
+        }
     }
 
-    if (until<2) {
-        printf("Wrong interval top value!");
-        return 0;
+    if (threadcount<threadstart) {
+        usage(argv[0], "\nInvalid --tmax value!\n--tmax should be greater or equal then --tmin\n\n");
+    }
+
+
+    printf("PrimeBenchc %s\n", _PRIMEBENCHC_VERSION);
+    if (listlicense) {
+        printf("All primebenchc code is Copyright 2013 by Nyiro Zoltan-Csaba.\n\nThis program is free software; you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation; either version 2 of the License, or (at\nyour option) any later version.\n\nThis program is distributed in the hope that it will be useful, but\nWITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY\nor FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License\nfor more details.\n\n");
+        printf("You should have received a copy of the GNU General Public License\nalong with this program as the file LICENSE.txt; if not, please see\nhttp://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.\n\n\n");
     }
     printf("The calculations are made up to %d thread(s).\n", threadcount);
     int i;
